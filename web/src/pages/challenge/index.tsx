eb43f79db070b8307cba43d10f2f1be13dd1368c
joseph0926 <joseph0926@naver.com>
2024-06-01 20:56:59 +0900

    feat/web: [6] 공통 컴포넌트 구축 - Button

--- web/src/pages/challenge/index.tsx
+++ web/src/pages/challenge/index.tsx
@@ -1,5 +1,13 @@
+import { Button } from '@/components/ui/Button';
+
 const Challenge = () => {
-  return <div>Challenge</div>;
+  return (
+    <div>
+      <Button variant="outline" size="sm" type="button" className="flex-center">
+        <h3>테스트</h3>
+      </Button>
+    </div>
+  );
 };
 
 export default Challenge;
