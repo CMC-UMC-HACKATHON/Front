604ffcfcc557793ae34f22e3daae6fbba6c47333
JunYoungKr <kop981020@naver.com>
2024-06-02 03:45:40 +0900

    feat:mypage css 설정

--- web/src/components/profile/Complete.tsx
+++ web/src/components/profile/Complete.tsx
@@ -1,13 +1,38 @@
 import * as React from 'react';
 import styles from '../profile/Complete.module.css';
-import WorkingSVg from '../../assets/working.svg?react';
+import WorkingSmallSVg from '../../assets/workingSmall.svg?react';
 
 const Complete = () => {
   return (
     <div>
       <h3 className={styles.h3}>완료된 챌린지</h3>
-      <div>
-        <WorkingSVg />
+      <div className={styles.container}>
+        <WorkingSmallSVg />
+        <div className={styles.container1}>
+          <div className={styles.container2}>
+            <h3 style={{ margin: '0' }}>디지털 보톡스 가바</h3>
+            <h4
+              style={{ margin: '0', color: 'var(--gray2)', fontWeight: '400' }}
+            >
+              2024.06.01 - 2024.08.02
+            </h4>
+          </div>
+          <h4 className={styles.h4}>달성률 100%</h4>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <WorkingSmallSVg />
+        <div className={styles.container1}>
+          <div className={styles.container2}>
+            <h3 style={{ margin: '0' }}>디지털 보톡스 가바</h3>
+            <h4
+              style={{ margin: '0', color: 'var(--gray2)', fontWeight: '400' }}
+            >
+              2024.06.01 - 2024.08.02
+            </h4>
+          </div>
+          <h4 className={styles.h4}>달성률 100%</h4>
+        </div>
       </div>
     </div>
   );
