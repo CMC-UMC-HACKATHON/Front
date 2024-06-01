c00b72703e42ee9e1877422382ba3d4b193357b1
JunYoungKr <kop981020@naver.com>
2024-06-01 22:53:28 +0900

    feat: ChallengeDetails css 설정

--- web/src/pages/challenge/details/ChallengeDetails.tsx
+++ web/src/pages/challenge/details/ChallengeDetails.tsx
@@ -1,5 +1,17 @@
+import React, { useState } from 'react';
+import styles from '../details/ChallengeDetails.module.css';
+import CalenderSvg from '../../../assets/calender.svg?react';
+import ChallengeHeader from '@/components/challenge/ChallengeHeader';
+import ChallengeDetailsBody from '@/components/challengeDetails/ChallengeDetailsBody';
+
 const ChallengeDetails = () => {
-  return <div>ChallengeDetails</div>;
+  return (
+    <div>
+      <ChallengeHeader />
+      {/* <div className={styles['body-container']}></div> */}
+      <ChallengeDetailsBody />
+    </div>
+  );
 };
 
 export default ChallengeDetails;
