03f4ebeacf0a339f2c1324e2b98fee0e8ec21ff1
joseph0926 <joseph0926@naver.com>
2024-06-02 02:00:50 +0900

    feat/common: [17] 스플래쉬 이미지 추가

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import NavigationBar from './src/components/NavigationBar';
 import {NavigationContainer} from '@react-navigation/native';
 import COLORS from './src/styles/colors';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen?.hide();
+    }, 1000);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
